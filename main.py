97716538491f3b8cc3dfd4e401a53a069a6eedc0
Drake Perrior-Small <drakecsmall@gmail.com>
2021-08-25 10:49:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -258,7 +258,7 @@ async def names(ctx, member: discord.Member):
     if len(result) == 0:
         await ctx.send("No previous names found for user")
     else:
-        await ctx.send("Account Change | Old Name\n" + "\n".join([f"{'true' if name[0] == 1 else 'false'} | {name[1]}" for name in results])
+        await ctx.send("Account Change | Old Name\n" + "\n".join([f"{'true' if name[0] == 1 else 'false'} | {name[1]}" for name in results]))
   
 @bot.event
 async def on_raw_reaction_add(payload):
